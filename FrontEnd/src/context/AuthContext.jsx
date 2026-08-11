513c4e270cb27f1d761a70206bcfdae272b58208
sayedmz <sayedmz9999@gmail.com>
2026-08-11 23:25:24 +0300

    AuthContext

--- FrontEnd/src/context/AuthContext.jsx
+++ FrontEnd/src/context/AuthContext.jsx
@@ -1,9 +1,38 @@
-import { createContext, useContext, useState } from "react";
+// import { createContext, useContext, useState } from "react";
+
+// const AuthContext = createContext(null);
+
+// export function AuthProvider({ children }) {
+//   const [user, setUser] = useState(null);
+
+//   return (
+//     <AuthContext.Provider value={{ user, setUser }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// }
+
+// export function useAuth() {
+//   return useContext(AuthContext);
+// }
+import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    const savedUser = localStorage.getItem("user");
+
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
+
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+  }, [user]);
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
